c20e571630d9187f98a49ea66a637b34115b3acb
brisstone <okolijohnson69@gmail.com>
2021-10-21 05:37:57 +0100

    initial

---
+++ build/precache-manifest.929503da22e4cc0bdb3ffa0a95621b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361ba03f037b4fe8cc528353bf9813b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4c437f884c2802316cd",
+    "url": "/static/css/2.1ba447cc.chunk.css"
+  },
+  {
+    "revision": "f98d7917b591cec269c2",
+    "url": "/static/css/main.835f18b1.chunk.css"
+  },
+  {
+    "revision": "b4c437f884c2802316cd",
+    "url": "/static/js/2.fe036289.chunk.js"
+  },
+  {
+    "revision": "dff38bf37fc9eda4acb236ebba3264fd",
+    "url": "/static/js/2.fe036289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f98d7917b591cec269c2",
+    "url": "/static/js/main.dbd056d5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
